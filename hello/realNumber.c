608d9d4818d84ebf3db80a67bb294b0d7f546f09
jhcode33 <jhcode33@gmail.com>
2024-04-26 00:48:21 +0900

    real number study

---
+++ hello/realNumber.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <float.h>
+
+int main()
+{
+    float n1 = FLT_MIN;
+    float n2 = FLT_MAX;
+    double n3 = DBL_MIN;
+    double n4 = DBL_MAX;
+    long double n5 = LDBL_MIN;
+    long double n6 = LDBL_MAX;
+
+    printf("%0.40f %.2f\n", n1, n2);
+    printf("%e %e\n", n3, n4);
+    printf("%Le %Le\n", n5, n6);
+}
