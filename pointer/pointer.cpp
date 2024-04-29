e3d9e03bf74f1802ab8a07a46fc2d0492bc54fa4
jhcode33 <jhcode33@gmail.com>
2024-04-30 00:45:00 +0900

    pointer

---
+++ pointer/pointer.cpp
@@ -0,0 +1,13 @@
+﻿#include <stdio.h>
+
+int main()
+{
+    int* numPtr; // 포인터 변수
+    int num1 = 10;
+    numPtr = &num1; // num1의 메모리 주소를 포인터 변수에 할당
+
+    printf("%p\n", numPtr);
+
+    printf("%p\n", &num1);
+    return 0;
+}
